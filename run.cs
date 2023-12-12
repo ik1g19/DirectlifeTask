f7c643b8336cd22c53437fa064bec57ee57452f9
ik1g19 <isaacklugman@gmail.com>
2023-12-12 18:48:58 +0000

    add comments

--- run.cs
+++ run.cs
@@ -2,15 +2,18 @@ using System;
 using System.IO;
 using System.Linq;
 
+// read and parse txt
 string filePath = "./names.txt";
 string[] names = File.ReadAllText(filePath)
                     .Replace("\"", "")
                     .Split(',');
 
+// alphabetically sort
 Array.Sort(names);
 
 long totalScore = 0;
 
+// iterate and multiply sum of letters by names position
 for (int i = 0; i < names.Length; i++)
 {
     int nameValue = GetNameValue(names[i]);
@@ -20,6 +23,11 @@ for (int i = 0; i < names.Length; i++)
 Console.WriteLine("Total Name Score: " + totalScore);
 
 
+/// <summary>
+/// Calculate sum of letter values in name e.g. A-1 B-2...
+/// </summary>
+/// <param name="name">Name to calculate sum of letters</param>
+/// <returns>Sum of letter values of given name</returns>
 static int GetNameValue(string name)
 {
     return name.Sum(c => char.ToUpper(c) - 'A' + 1);
